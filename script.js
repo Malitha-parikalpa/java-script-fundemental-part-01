9be618584428df8293011a1045c9afd4c7e12c91
malitha <it22324238@my.sliit.lk>
2024-12-01 12:10:29 +0530

    change

--- script.js
+++ script.js
@@ -1,9 +1,70 @@
-let js = 'amazing';
+// let js = 'amazing';
 
-if(js==='amazing') alert("Hello");
-console.log(10+22/2+44-2)
+// // if(js==='amazing') alert("Hello");
+// // console.log(10+22/2+44-2)
 
-let _name="maliboy";
-let $name="parikalpa";
+// // let _name="maliboy";
+// // let $name="parikalpa";
 
-console.log(_name,$name);
+// // console.log(_name,$name);
+
+
+// let country = "Sri lanka";
+// const contient = 'Europe';
+// const population='10';
+// const isIsland = true;
+// // isIsland = true;
+
+// console.log( country);
+// console.log(contient);
+
+// console.log(typeof population)
+// console.log( isIsland);
+
+
+// console.log( population/2);
+
+
+// console.log(population>6);
+// console.log(population<6);
+
+// true;
+
+
+// console.log(hi);
+// let hi= true;
+
+// let java;
+// console.log(typeof java);
+// java = 1991;
+// console.log(typeof java);
+// var hi;
+// console.log(typeof hi);
+
+
+
+// const Mark_weight = 78;
+// const Mark_height = 1.69;
+// const john_weight = 92;
+// const john_height = 1.95;
+
+// const BMI_mark = Mark_weight/Mark_height**2;
+// const BMI_john = john_weight/(john_height*john_height);
+// const markHigherBMI = BMI_mark>BMI_john;
+
+// console.log(BMI_mark,BMI_john,markHigherBMI);
+
+
+
+const myname = 'malitha';
+const job = 'software Engineer';
+const birth_year = '2001';
+const current_year = '2024';
+
+const malitha = "I'm "+myname + " My job is "+job+" age is "+(current_year-birth_year);
+console.log(malitha);
+const newMalitha = "I am "+ myname;
+const newMalitha1 = `I'm ${myname}, a ${current_year-birth_year} year old`
+
+console.log(newMalitha);
+console.log(newMalitha1);
